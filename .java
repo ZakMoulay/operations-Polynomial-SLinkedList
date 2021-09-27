fb645fa8b885567d0c86ed6dca113473d52bf1b7
ZakMoulay <74928710+ZakMoulay@users.noreply.github.com>
2021-09-27 14:19:11 -0400

    Update .java

--- .java
+++ .java
@@ -167,3 +167,99 @@ public class Polynomial {
 
         }
     }
+    
+    public class SinglyLinkedList<T> {
+    private Node<T> head,tail;
+    private int size;
+    public SinglyLinkedList() {
+        head = tail = null;
+        size = 0;
+    }
+    public int getSize() { return size; }
+    public boolean isEmpty() { return size == 0; }
+    public T getHead() {
+        if ( head == null )
+            return null;
+        return head.getData(); }
+
+
+
+    public void addFirst(T d) {
+        Node<T> newNode = new Node<T>(d, head);
+        head = newNode;
+        if (size == 0)
+            tail = head;
+        size++;
+    }
+    public void addLast(T d){
+        Node<T> newNode = new Node<T>(d,null);
+        if ( isEmpty()) head = tail = newNode;
+        else {
+            tail.setNext(newNode);
+            tail = newNode;
+        }
+    }
+        
+public class Node<T> {
+    private T data;
+    private Node<T> next;
+
+    public Node() {
+        data = null;
+        next = null;
+    }
+
+    public Node(T d, Node<T> n) {
+        data = d;
+        next = n;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setData(T d) {
+        data = d;
+    }
+
+    public void setNext(Node<T> n) {
+        next = n;
+    }
+
+    public String toString() {
+        String ans = "";
+        //Node<T> node = this;
+        if (this != null) {
+            ans += this.data;
+            Node<T> node = this.next;
+            if (node != null) ans = ans + "-->" + node.data;
+        }
+        return ans;
+
+    }
+}
+        
+public class Term {
+
+
+        private double coeff ;
+        private int pow;
+        public Term() {
+            coeff = 0;
+            pow = 0;
+        }
+        // add constructors and auxiliary methods
+        public Term(double c, int p) {
+            coeff = c;
+            pow = p;
+        }
+        public double getCoeff() { return coeff; }
+        public int getPow() { return pow; }
+
+        public void setCoeff(double c) { coeff = c;}
+        public void setPow(int p) { pow = p; }
+}
